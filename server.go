614b7a75b95a7089b41ffc945228def9661d5ee4
Murray Resinski <murray.resinski@octanner.com>
2018-06-21 11:57:39 -0600

    COBRA-1905 - add region to variables.

--- server.go
+++ server.go
@@ -17,9 +17,12 @@ import (
 	"os"
 )
 
+var region string
+
 func main() {
 	brokerdb := utils.Init()
 	octdb.Init(brokerdb)
+        region = os.Getenv("REGION")
 	m := martini.Classic()
 	m.Use(render.Renderer())
 	m.Get("/v1/s3/plans", plans)
@@ -34,7 +37,7 @@ func main() {
 func url2(params martini.Params, r render.Render) {
 	rlocation, raccesskey, rsecretkey := octdb.Retrieve(params["name"])
 
-	r.JSON(200, map[string]string{"S3_BUCKET": params["name"], "S3_LOCATION": rlocation, "S3_ACCESS_KEY": raccesskey, "S3_SECRET_KEY": rsecretkey})
+	r.JSON(200, map[string]string{"S3_BUCKET": params["name"], "S3_LOCATION": rlocation, "S3_ACCESS_KEY": raccesskey, "S3_SECRET_KEY": rsecretkey,"S3_REGION":region})
 }
 
 func createname() string {
@@ -87,6 +90,7 @@ func provision(spec structs.Provisionspec, err binding.Errors, r render.Render) 
         s3spec.Bucket = basename
 	s3spec.Accesskey = s3user.Accesskey
 	s3spec.Secretkey = s3user.Secretkey
+        s3spec.Region = region
 	octdb.Store(s3user.Username, s3spec.Location, s3spec.Accesskey, s3spec.Secretkey, spec.Billingcode)
 	r.JSON(200, s3spec)
 }
